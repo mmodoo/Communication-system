29c2bba54ade6ee3d853bd423c1507320a0832d5
문기옥 <okmunkiok@korea.ac.kr>
2024-11-28 14:07:14 +0900

    Delete test/QAM8 directory

--- test/QAM8/test_Tx/Tx_Step_6_Add_Preamble.m
+++
@@ -1,9 +0,0 @@
-function Tx_signal = Tx_Step_6_Add_Preamble(Tx_signal, Whther_Only_Preamble_1_Chirp__OR__plus_Preamble_2, Preamble_1_Chirp)
-
-    if Whther_Only_Preamble_1_Chirp__OR__plus_Preamble_2 == true
-        Preamble = Preamble_1_Chirp;
-    else
-    end
-
-    Tx_signal = [Preamble; Tx_signal];
-end
--- test/QAM8/test_Tx/Tx_Test_1_function.m
+++
@@ -1,4 +0,0 @@
-function [a, b] = Tx_Test_1_function()
-    a = 1;
-    b = 2;
-end
